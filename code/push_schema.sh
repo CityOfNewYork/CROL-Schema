3709758edcf6c0151588a149ac199a5f1e2a7a03
Amal Sudama <cds.sudama@gmail.com>
2015-04-15 07:12:10 -0400

    deploy

--- code/push_schema.sh
+++ code/push_schema.sh
@@ -1,13 +1,11 @@
 #!env bash
 
+DEPLOY_DIR="$(dirname `pwd`)/deploy"
+APP_DIR="$(dirname `pwd`)/browser"
 
 # write new schema in deploy and local folder
-[ -f ../browser/docson/crow-schema.json ] && rm ../browser/deploy/docson/schema.json
-`which python3` ./schema.py > ../browser/deploy/docson/schema.json
+[ -f $DEPLOY_DIR/docson/crow-schema.json ] && rm $DEPLOY_DIR/docson/schema.json
+python ./schema.py > $DEPLOY_DIR/docson/schema.json
 
 # copy to local
-cp ../browser/deploy/docson/schema.json ../browser/docson
-
-(cd ../browser/deploy && git commit -am "deploy" && git push heroku master && heroku open)
-
-
+(cd $DEPLOY_DIR && git commit -am "deploy" && git push heroku master && heroku open)
