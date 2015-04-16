73345a7f228c026a190a0ef01955dd872a0cfbd3
Amal Sudama <cds.sudama@gmail.com>
2015-04-16 15:32:44 -0400

    fix reference

--- code/entities/__init__.py
+++ code/entities/__init__.py
@@ -3,11 +3,11 @@ from .noticeData    import NoticeData
 from .skeleton      import Skeleton
 
 #Attributes
-from attributeList 	import Attributes
+from .attributeList 	import Attributes
 from .organization 	import Organization
 from .address		import Address
 from .meeting		import Meeting
-	
+
 #Schemas
 from .schemaList    import Schemas
 
