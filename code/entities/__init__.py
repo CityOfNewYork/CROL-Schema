70fd22e035b83c6f47875934e13091dc59e0a189
Mikael MH <team@commune.io>
2015-04-16 15:12:23 -0400

    update

--- code/entities/__init__.py
+++ code/entities/__init__.py
@@ -3,18 +3,11 @@ from .noticeData    import NoticeData
 from .skeleton      import Skeleton
 
 #Attributes
-<<<<<<< HEAD
 from attributeList 	import Attributes
 from .organization 	import Organization
 from .address		import Address
 from .meeting		import Meeting
 	
-=======
-from .attributeList import Attributes
-from .organization  import Organization
-from .address       import Address
-
->>>>>>> origin/master
 #Schemas
 from .schemaList    import Schemas
 
